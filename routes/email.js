22df4da82a99c5a9db0929d9cb9eed0538673703
Harry <harold.william.bell@gmail.com>
2019-01-21 16:11:51 -0700

    first commit, has email functionality but nothing else

---
+++ routes/email.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var router = express.Router();
+const nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'famri.improve.emailer@gmail.com',
+    pass: process.env.gPass
+  }
+})
+
+/* GET users listing. */
+router.post('/', function(req, res, next) {
+  console.log('Handling post request')
+  console.log(`Request Body: ${req.body.contact}`);
+
+  var mailOptions = {
+    from: 'famri.improve.emailer.com',
+    to: 'harold.william.bell@gmail.com',
+    subject: `New Contact: ${req.body.name}`,
+    text: `Hi, my name is ${req.body.name}. ${req.body.message} My email is ${req.body.email}`
+  }
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(`Email sent: ${info.response}`);
+      console.log(`New Contact: ${req.body.name}`);
+      console.log(`Message: ${req.body.message}`);
+      console.log(`Email: ${req.body.email}`)
+    }
+  })
+
+  let message = {
+    body: 'Thank you for your information!'
+  }
+  res.json(message);
+});
+
+module.exports = router;
